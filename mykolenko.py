fa55cc884b180dbf54395674311239adda3ec5b8
Mykolenko08 <v.mykolenko_fit_16_25_b_d@knute.edu.ua>
2025-11-25 17:30:59 +0200

    Мій перший commit

---
+++ mykolenko.py
@@ -0,0 +1,37 @@
+﻿def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# my_list = [64, 34, 25, 12, 22, 11, 90]
+
+# print("Bubble Sort:", bubble_sort(my_list.copy()))
+# print("Selection Sort:", selection_sort(my_list.copy()))
+# print("Insertion Sort:", insertion_sort(my_list.copy()))
